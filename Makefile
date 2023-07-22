ef119a9b256f20a12f26521bd99da4b8af233149
Helgy <oleglavro6@gmail.com>
2023-07-22 12:17:12 +0300

    feat: Makefiles reorganization

--- Makefile
+++ Makefile
@@ -1,9 +1,7 @@
-
 all:
 	@mkdir -p build
 	$(MAKE) -C FileSearcher;
 	$(MAKE) -C app;  
 
-# Clean rule
 clean:
 	rm -rf build
--- common.mk
+++ common.mk
@@ -1,2 +1,10 @@
 CXX := g++
 CXXFLAGS := -Wall -Wextra -std=c++17
+
+LIB_NAME := FileSearcherLib.a
+
+SRC_DIR := src
+BUILD_DIR := ../build
+
+SRC_FILES := $(wildcard $(SRC_DIR)/*.cpp)
+OBJ_FILES := $(patsubst $(SRC_DIR)/%.cpp,$(BUILD_DIR)/%.o,$(SRC_FILES))
